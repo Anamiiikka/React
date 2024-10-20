7fdde512b04e711a5f29811e7839f893b6a582eb
Anamika <anamikasingh200205@gmail.com>
2024-10-21 01:40:13 +0530

    commit done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Welcome to React
+
+# Parcel
+-Dev Build
+-Local Server
+-HMR-Hot Module Replacements
+-Uses a File Watching Algorithm - written in Cpp
+-Image Optimisation
+-Caching-Faster Builds
+-Minification of files 
+-Bundling of Files
+-Compress the files
+-Consistent Hashing
+-Code Splitting
+-Differential Bundling-to support older browsers
+-Diagnostics behind the scenes
+-Good error handling
+-Gives a way to host on https
+-Tree Shaking(remove unused code)
---
+++ src/components/Body.js
@@ -0,0 +1,43 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState,useEffect } from "react";
+
+const Body = () => {
+  const [listOfRestaurants, useListOfRestaurants] = useState(resList);
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  const fetchData = async ()=>{
+  const data= await fetch(
+  "https://www.swiggy.com/dapi/restaurants/list/v5/?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+
+  const json=data.json();
+  useListOfRestaurants(json);
+  };
+  
+  
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="btn-filter"
+          onClick={() => {
+            const filteredList = resList.filter(
+              (res) => res.data.avgRating > 4
+            );
+            useListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
